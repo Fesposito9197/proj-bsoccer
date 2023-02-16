78bf88ae9a59b9af288d0acfe637c8f7482992c8
Fesposito9197 <kekkoesp8@gmail.com>
2023-02-16 18:35:40 +0100

    creazione table player con seeder in tabella

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,12 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+
+    public function player()
+    {
+        return $this->hasOne(Player::class);
+    }
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
---
+++ database/migrations/2023_02_16_153304_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('profile_photo');
+            $table->string('phone_number', 50);
+            $table->text('description');
+            $table->date('birth_date');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+};
