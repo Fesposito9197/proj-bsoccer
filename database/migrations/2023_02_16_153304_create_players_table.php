888316efb8574816fa4da071567d3ac94a9d0ffe
Fesposito9197 <kekkoesp8@gmail.com>
2023-02-21 10:43:10 +0100

    aggiunge profile photo nullable

--- database/migrations/2023_02_16_153304_create_players_table.php
+++ database/migrations/2023_02_16_153304_create_players_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('players', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('profile_photo');
+            $table->string('profile_photo')->nullable();
             $table->string('phone_number', 50);
             $table->text('description');
             $table->date('birth_date');
