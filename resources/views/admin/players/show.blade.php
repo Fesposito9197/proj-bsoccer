e71ba5613061c276c602224b3f5ea3d73580be14
danifaso02 <danifaso02@gmail.com>
2023-02-22 09:41:12 +0100

    Modifica stile show
    Co-authored-by: Fesposito9197 <Fesposito9197@users.noreply.github.com>

--- resources/views/admin/players/show.blade.php
+++ resources/views/admin/players/show.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        @if (!empty($player))
-            <h1>{{ $player->user->name }}</h1>
-            <h1>{{ $player->city }}</h1>
+    <div class="container bg-success p-3 text-white" style="height: 100vh">
+        @if (!empty($player))    
+            <h2>Nome: {{ $player->user->name }}</h2>
+            <h2 class="my-3">Città: {{ $player->city }}</h2>
+            <h2 class="mb-3">Descrizione: {{ $player->description }}</h2>
 
+            <h2>Ruoli:</h2>
             @foreach ($player->roles as $role)
-                <span class="badge rounded-pill text-bg-dark"><a href="#"
+                <span id="span_show" class="badge rounded-pill text-bg-dark py-2 my-2 mx-1 col-2"><a href="#"
                         class="text-white text-decoration-none">{{ $role->name }}</a></span>
             @endforeach
+            
         @else
             <a href="{{ route('admin.players.create') }}" class="btn btn-success"><i
                     class="fa-regular fa-square-plus fa-lg fa-fw"></i></a>
