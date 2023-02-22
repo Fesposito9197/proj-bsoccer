2d6846615e5e41088807e1c47b6df3444cc159ae
danifaso02 <danifaso02@gmail.com>
2023-02-22 17:03:58 +0100

    modifica stile index-messages and reviews

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container mt-4">
+<div class="container mt-4 d-flex">
     @foreach ($messages as $message)
-    <ul>
-        <li>
-            {{$message->name}}
-            <p>{{$message->email}}</p>
-            <p>{{$message->content}}</p>
-            <p>{{$message->date_message}}</p>
-        </li>
-    </ul>
+        <ul class="list-group">
+            <li class="d-flex me-3">
+                <div class="card border-secondary border border-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5>{{$message->name}}</h5>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-secondary">{{$message->email}}</p>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-secondary">{{$message->content}}</p>
+                        <p class="text-end">{{$message->date_message}}</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
     @endforeach
 </div>
-    
-
-    
-    
 @endsection
--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container mt-4">
+<div class="container mt-4 d-flex">
     @foreach ($reviews as $review)
-        <ul>
-            <li>
-                {{$review->name}}
-                <p>{{$review->content}}</p>
-                <p>{{$review->date_message}}</p>
+        <ul class="list-group">
+            <li class="d-flex me-3">
+                <div class="card border-info border border-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5>{{$review->name}}</h5>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-info">{{$review->email}}</p>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-info">{{$review->content}}</p>
+                        <p class="text-end">{{$review->date_message}}</p>
+                    </div>
+                </div>
             </li>
         </ul>
     @endforeach
 </div>
 @endsection
+
