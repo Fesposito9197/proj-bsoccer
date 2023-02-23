0ed217dd6e980c1593c853f983c490029901d7c0
danifaso02 <danifaso02@gmail.com>
2023-02-23 13:42:53 +0100

    Modifica mex e rev

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -4,12 +4,12 @@
 <div class="container mt-4 d-flex">
     @foreach ($messages as $message)
         <ul class="list-group">
-            <li class="d-flex me-3">
+            <li class="d-flex me-3" style="height: 100%;">
                 <div class="card border-secondary border border-3" style="width: 18rem;">
                     <div class="card-body">
                         <h5>{{$message->name}}</h5>
                         <p class="border-bottom pb-2 mb-2 border-3 border-secondary">{{$message->email}}</p>
-                        <p class="border-bottom pb-2 mb-2 border-3 border-secondary">{{$message->content}}</p>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-secondary" style="min-height: 200px; max-height: 200px; overflow-x: auto;">{{$message->content}}</p>
                         <p class="text-end">{{$message->date_message}}</p>
                     </div>
                 </div>
--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.admin')
+
 @section('content')
 <div class="container mt-4 d-flex">
     @foreach ($reviews as $review)
         <ul class="list-group">
-            <li class="d-flex me-3">
+            <li class="d-flex me-3" style="height: 100%;">
                 <div class="card border-info border border-3" style="width: 18rem;">
                     <div class="card-body">
                         <h5>{{$review->name}}</h5>
                         <p class="border-bottom pb-2 mb-2 border-3 border-info">{{$review->email}}</p>
-                        <p class="border-bottom pb-2 mb-2 border-3 border-info">{{$review->content}}</p>
+                        <p class="border-bottom pb-2 mb-2 border-3 border-info" style="min-height: 200px; max-height: 200px; overflow-x: auto;">{{$review->content}}</p>
                         <p class="text-end">{{$review->date_message}}</p>
                     </div>
                 </div>
@@ -17,4 +18,3 @@
     @endforeach
 </div>
 @endsection
-
